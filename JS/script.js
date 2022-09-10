c4b9b89f9b9d54ffa356b2b88602d2a666ce69e0
RobicaCodeaza <stoicarobert38@yahoo.com>
2022-09-11 01:12:42 +0300

    Added cta section

--- JS/script.js
+++ JS/script.js
@@ -18,14 +18,14 @@ var swiper = new Swiper(".slide-content", {
   },
 });
 
-// const signUpButton = document.getElementById('signUp');
-// const signInButton = document.getElementById('signIn');
-// const container = document.getElementById('container');
+const signUpButton = document.querySelector(".btn--form-cover-account");
+const signInButton = document.querySelector(".btn--form-cover-sign");
+const container = document.querySelector(".container--form");
 
-// signUpButton.addEventListener('click', () => {
-// 	container.classList.add("right-panel-active");
-// });
+signUpButton.addEventListener("click", () => {
+  container.classList.add("coverall");
+});
 
-// signInButton.addEventListener('click', () => {
-// 	container.classList.remove("right-panel-active");
-// });
+signInButton.addEventListener("click", () => {
+  container.classList.remove("coverall");
+});
