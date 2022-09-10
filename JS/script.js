7a780b23b81ac0129a5dfd61d9137f4526c34efa
RobicaCodeaza <stoicarobert38@yahoo.com>
2022-09-10 15:41:36 +0300

    added changes to js

--- JS/script.js
+++ JS/script.js
@@ -17,3 +17,15 @@ var swiper = new Swiper(".slide-content", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+// const signUpButton = document.getElementById('signUp');
+// const signInButton = document.getElementById('signIn');
+// const container = document.getElementById('container');
+
+// signUpButton.addEventListener('click', () => {
+// 	container.classList.add("right-panel-active");
+// });
+
+// signInButton.addEventListener('click', () => {
+// 	container.classList.remove("right-panel-active");
+// });
